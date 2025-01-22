2422215a91875406f9396762e07f02eb91d41635
Ashish Prasad <ashish.ap.rao@gmail.com>
2025-01-22 10:22:22 +0530

    Added PVS ( nodes 1001593 nps 1674904 )

--- src/search.cpp
+++ src/search.cpp
@@ -95,53 +95,23 @@ namespace sagittar {
 
             const i32 alpha_orig = alpha;
 
+            const bool is_in_check = movegen::isInCheck(board);
+
             if (board.getPlyCount() >= MAX_DEPTH) [[unlikely]]
             {
                 return eval::evaluateBoard(board);
             }
-
             if (board.hasPositionRepeated() || board.getHalfmoveClock() >= 100)
             {
                 return 0;
             }
-
-            const bool is_in_check = movegen::isInCheck(board);
-
             if (is_in_check)
             {
                 depth++;
             }
-
             if (depth <= 0)
             {
                 return quiescencesearch(board, alpha, beta, info, result);
-            }
-
-            constexpr bool is_pv_node_type = (nodeType != NodeType::NON_PV);
-            const bool     is_pv_node      = ((beta - alpha) > 1) || is_pv_node_type;
-
-            if (board.getPlyCount() > 0 && !is_pv_node)
-            {
-                tt::TTEntry ttentry;
-                const bool  tthit = tt.probe(&ttentry, board);
-                if (tthit && ttentry.depth >= depth)
-                {
-                    i32 ttvalue = ttentry.value;
-
-                    if (ttvalue < -MATE_SCORE)
-                    {
-                        ttvalue += board.getPlyCount();
-                    }
-                    else if (ttvalue > MATE_SCORE)
-                    {
-                        ttvalue -= board.getPlyCount();
-                    }
-
-                    if (ttentry.flag == tt::TTFlag::EXACT)
-                    {
-                        return ttvalue;
-                    }
-                }
             }
 
             i32        best_score = -INF;
