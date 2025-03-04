d9b0bef1a8f396a2e0410596cf9153c64924ac8d
Ashish Prasad <ashish.ap.rao@gmail.com>
2025-03-04 20:45:51 +0530

    Removed tests ( nodes 293039 nps 1054097 )

--- test/test_params.cpp
+++ test/test_params.cpp
@@ -6,16 +6,5 @@ using namespace sagittar;
 
 TEST_SUITE("Params") {
 
-    TEST_CASE("Params::set and Params::get") {
-        const float EPSILON = 0.0001;
-
-        parameters::ParameterStore params;
-
-        params.set<int>("FOO", 1);
-        params.set<float>("BAR", 3.14);
-
-        REQUIRE(params.get<int>("FOO", 0) == 1);
-        REQUIRE(std::abs(params.get<float>("BAR", 0.0) - 3.14) <= EPSILON);
-        REQUIRE(std::abs(params.get<float>("BAT", 1.2345) - 1.2345) <= EPSILON);
-    }
+    TEST_CASE("Params set") {}
 }
