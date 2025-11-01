97fc9581dd204c6ad499af19a699e3987067553e
Ashish Prasad <ashish.ap.rao@gmail.com>
2025-11-01 10:04:08 +0530

    Refactored namespaces; removed dead code ( nodes 187971 nps 882492 os darwin )

---
+++ src/search/types.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "commons/pch.h"
+#include "core/move.h"
+#include "core/types.h"
+
+namespace sagittar::search {
+
+    struct SearchInfo {
+        // Inputs
+        bool  infinite;
+        u32   wtime, btime, winc, binc, movetime, movestogo;
+        Depth depth;
+        // Set by timeman
+        bool timeset;
+        u64  starttime, stoptime;
+
+        SearchInfo() :
+            infinite(false),
+            wtime(0),
+            btime(0),
+            winc(0),
+            binc(0),
+            movetime(0),
+            movestogo(0),
+            depth(0),
+            timeset(false),
+            starttime(0ULL),
+            stoptime(0ULL) {}
+    };
+
+    struct SearchResult {
+        Score             score;
+        bool              is_mate;
+        i8                mate_in;
+        Depth             depth;
+        size_t            nodes;
+        u64               time;
+        u32               hashfull;
+        std::vector<Move> pv;
+        Move              bestmove{};
+    };
+
+}
