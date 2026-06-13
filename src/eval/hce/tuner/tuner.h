b26922829676cd4b50a775fa6a3123e989c88647
Ashish Prasad <ashish.ap.rao@gmail.com>
2026-06-14 00:27:39 +0530

    Retry with changing hyperparams ( nodes 189057 nps 1454284 os darwin )

--- src/eval/hce/tuner/tuner.h
+++ src/eval/hce/tuner/tuner.h
@@ -5,16 +5,16 @@
 namespace sagittar::eval::hce::tuner {
 
     struct TunerSettings {
-        bool   compute_k                   = true;
+        bool   compute_k                   = false;
         bool   retune_from_zero            = false;
-        double K                           = 2.9;
-        size_t epochs                      = 10000;
+        double K                           = 1.0;
+        size_t epochs                      = 3000;
         double beta1                       = 0.9;
         double beta2                       = 0.999;
-        double learning_rate_init          = 0.15;
-        size_t learning_rate_drop_interval = 1000;
-        double learning_rate_drop_ratio    = 0.75;
-        size_t thread_count                = 4;  // std::thread::hardware_concurrency();
+        double learning_rate_init          = 0.01;
+        size_t learning_rate_drop_interval = 750;
+        double learning_rate_drop_ratio    = 0.5;
+        size_t thread_count                = 2;  // std::thread::hardware_concurrency();
     };
 
     void tune(const std::filesystem::path& epd_path, const TunerSettings& settings);
